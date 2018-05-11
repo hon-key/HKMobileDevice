a9b53dd83577bad8a4f9ce7f3210a73faa29880c
HJ-CAI <lj_chj@126.com>
2018-05-11 09:14:03 +0800

    first version

---
+++ HKMobileDevice/HKMobileDevice.h
@@ -0,0 +1,19 @@
+//
+//  HKMobileDevice.h
+//  HKMobileDevice
+//
+//  Created by 工作 on 2018/5/11.
+//  Copyright © 2018年 com.CAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HKMobileDevice.
+FOUNDATION_EXPORT double HKMobileDeviceVersionNumber;
+
+//! Project version string for HKMobileDevice.
+FOUNDATION_EXPORT const unsigned char HKMobileDeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HKMobileDevice/PublicHeader.h>
+
+
